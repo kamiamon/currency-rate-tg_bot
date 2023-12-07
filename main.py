dde6fdbaf0752a5f2d8d881cfa01a8b28d315c56
kamiamon <121360693+kamiamon@users.noreply.github.com>
2023-12-07 18:44:27 +0300

    Fully developed, there are little things left

--- main.py
+++ main.py
@@ -13,15 +13,6 @@ API_KEY = config("API_KEY_LAYER")
 
 SELECTING, CHOOSING_CURRENCY, CHOOSING_INTERVAL, SETTING_MIN_THRESHOLDS, SETTING_MAX_THRESHOLDS, MONITORING = range(6)
 
-selected_currency = None
-monitoring_interval = None
-
-rate_data = []
-time_data = []
-
-min_threshold = None
-max_threshold = None
-
 CACHE_FILE_PATH = 'rate_data_cache.json'
 
 def load_rate_data_from_cache():
@@ -37,16 +28,25 @@ def save_rate_data_to_cache(data):
 async def start(update: Update, context: CallbackContext):
     user = update.effective_user
     await update.message.reply_html(
-        f"\U0001F44B Привет, {user.mention_html()}!\n"
-        "Этот бот предназначен для мониторинга курсов валют.\n"
+        f"\U0001F44B Привет, {user.mention_html()}!\n\n"
+        "Этот бот предназначен для мониторинга курсов валют.\n\n"
         "Используйте /settings, чтобы настроить мониторинг."
     )
     return SELECTING
 
+
 async def cancel(update: Update, context: CallbackContext):
-    await update.message.reply_text("Мониторинг отменен.")
-    context.user_data.pop('job', None)
+    job = context.user_data.get('job')
+    if job:
+        job.cancel()
+
+    context.user_data.clear()
+
+    await update.message.reply_text("\U000026D4 Мониторинг отменен. Все настройки сброшены.\n\n"
+                                    "Используйте /settings, чтобы настроить мониторинг.")
+
     return ConversationHandler.END
+
 
 currencies_file_path = 'valid_currencies.json'
 
@@ -57,17 +57,16 @@ def is_valid_currency(currency):
     return currency.upper() in currencies_data.get('currencies', {})
 
 async def settings(update: Update, context: CallbackContext):
-    await update.message.reply_text("\U00002699 Выберите валюту, курс которой вы хотите мониторить (например, EUR, RUB, JPY):")
+    await update.message.reply_text("\U0001F4B5 Выберите валюту, курс которой вы хотите мониторить (например, EUR, RUB, JPY):")
     return CHOOSING_CURRENCY
 
 async def set_currency(update: Update, context: CallbackContext):
-    global selected_currency
     entered_currency = update.message.text.upper()
 
     if is_valid_currency(entered_currency):
-        selected_currency = entered_currency
+        context.user_data['selected_currency'] = entered_currency
         await update.message.reply_text(
-            f"\U00002705 Выбрана валюта: {selected_currency}.\n"
+            f"\U00002705 Выбрана валюта: {context.user_data['selected_currency']}.\n\n"
             "Теперь выберите интервал мониторинга в минутах (например, 15):"
         )
         return CHOOSING_INTERVAL
@@ -76,13 +75,12 @@ async def set_currency(update: Update, context: CallbackContext):
         return CHOOSING_CURRENCY
 
 async def set_interval(update: Update, context: CallbackContext):
-    global monitoring_interval
     try:
-        monitoring_interval = int(update.message.text)
-        if monitoring_interval <= 0:
+        context.user_data['monitoring_interval'] = int(update.message.text)
+        if context.user_data['monitoring_interval'] <= 0:
             raise ValueError("\U0000274C Интервал мониторинга должен быть положительным числом.")
         await update.message.reply_text(
-            f"\U000023F1 Выбран интервал мониторинга: {monitoring_interval} минут.\n"
+            f"\U000023F1 Выбран интервал мониторинга: {context.user_data['monitoring_interval']} минут.\n\n"
             "Введите минимальное значение курса:"
         )
         return SETTING_MIN_THRESHOLDS
@@ -91,10 +89,9 @@ async def set_interval(update: Update, context: CallbackContext):
         return CHOOSING_INTERVAL
 
 async def set_min_threshold(update: Update, context: CallbackContext):
-    global min_threshold
     try:
-        min_threshold = float((update.message.text).replace(',', '.'))
-        await update.message.reply_text(f"\U0001F4C9 Минимальное пороговое значение установлено: {min_threshold}.\n"
+        context.user_data['min_threshold'] = float((update.message.text).replace(',', '.'))
+        await update.message.reply_text(f"\U0001F4C9 Минимальное пороговое значение установлено: {context.user_data['min_threshold']}.\n\n"
                                         "Введите максимальное значение:")
         return SETTING_MAX_THRESHOLDS
     except ValueError:
@@ -102,10 +99,9 @@ async def set_min_threshold(update: Update, context: CallbackContext):
         return SETTING_MIN_THRESHOLDS
 
 async def set_max_threshold(update: Update, context: CallbackContext):
-    global max_threshold
     try:
-        max_threshold = float((update.message.text).replace(',', '.'))
-        await update.message.reply_text(f"\U0001F4C8 Максимальное пороговое значение установлено: {max_threshold}.\n"
+        context.user_data['max_threshold'] = float((update.message.text).replace(',', '.'))
+        await update.message.reply_text(f"\U0001F4C8 Максимальное пороговое значение установлено: {context.user_data['max_threshold']}.\n"
                                         "Используйте /monitor_start, чтобы начать мониторинг.")
         return ConversationHandler.END
     except ValueError:
@@ -113,6 +109,11 @@ async def set_max_threshold(update: Update, context: CallbackContext):
         return SETTING_MAX_THRESHOLDS
 
 async def monitor_start(update: Update, context: CallbackContext):
+    selected_currency = context.user_data.get('selected_currency')
+    monitoring_interval = context.user_data.get('monitoring_interval')
+    min_threshold = context.user_data.get('min_threshold')
+    max_threshold = context.user_data.get('max_threshold')
+
     if selected_currency:
         base_currency = "USD"
         symbols = selected_currency
@@ -129,9 +130,6 @@ async def monitor_start(update: Update, context: CallbackContext):
                         await update.message.reply_text(f"Error: {data['error']['info']}")
                     else:
                         rate = data['quotes'].get(f'USD{selected_currency}')
-                        rate_data.append(rate)
-                        time_data.append(str(datetime.now().strftime("%d-%m %H:%M")))
-
                         currency_cache_data = load_rate_data_from_cache()
                         if selected_currency not in currency_cache_data:
                             currency_cache_data[selected_currency] = {'rates': [], 'times': []}
@@ -139,15 +137,15 @@ async def monitor_start(update: Update, context: CallbackContext):
                         currency_cache_data[selected_currency]['times'].append(str(datetime.now().strftime("%d-%m %H:%M")))
                         save_rate_data_to_cache(currency_cache_data)
 
-                        await draw_graph()
+                        await draw_graph(update, context)
 
                         if min_threshold is not None and rate < min_threshold:
                             await update.message.reply_text(
-                                f"\u26A0 Внимание! Курс {selected_currency} преодолел минимальное пороговое значение: {rate}")
+                                f"\u26A0 Внимание!\n\nКурс {selected_currency} преодолел минимальное пороговое значение: {rate}")
 
                         if max_threshold is not None and rate > max_threshold:
                             await update.message.reply_text(
-                                f"\u26A0 Внимание! Курс {selected_currency} преодолел максимальное пороговое значение: {rate}")
+                                f"\u26A0 Внимание!\n\nКурс {selected_currency} преодолел максимальное пороговое значение: {rate}")
 
                 except Exception as e:
                     print(f"Error: {e}")
@@ -158,24 +156,14 @@ async def monitor_start(update: Update, context: CallbackContext):
         context.user_data['job'] = task
         await update.message.reply_text(
             f"\U0001F680 Мониторинг курса {selected_currency} начат с интервалом {monitoring_interval} минут.\n\n"
-            "Используйте команду /get_currency, чтобы узнать последнее значение курса.\n\n"
-            "Используйте команду /get_graph, чтобы получить график валюты.")
+            "Используйте команду /currency, чтобы узнать последнее значение курса.\n\n"
+            "Используйте команду /cancel, чтобы отменить мониторинг.")
     else:
-        await update.message.reply_text("Настройте мониторинг, используя /settings.")
+        await update.message.reply_text("\U00002699 Настройте мониторинг, используя команду /settings.")
     return SELECTING
+async def draw_graph(update: Update, context: CallbackContext):
+    selected_currency = context.user_data.get('selected_currency')
 
-async def get_currency(update: Update, context: CallbackContext):
-    if selected_currency:
-        currency_cache_data = load_rate_data_from_cache()
-        if selected_currency in currency_cache_data:
-            last_rate = currency_cache_data[selected_currency]['rates'][-1]
-            await update.message.reply_text(f"\U0001F3C1 Последнее значение курса {selected_currency}: {last_rate}")
-        else:
-            await update.message.reply_text("\U0000274C Нет данных о курсе. Начните мониторинг с помощью /monitor_start.")
-    else:
-        await update.message.reply_text("\U0000274C Не выбрана валюта для мониторинга. Используйте /settings.")
-
-async def draw_graph():
     if selected_currency:
         currency_cache_data = load_rate_data_from_cache()
         if selected_currency in currency_cache_data:
@@ -193,10 +181,21 @@ async def draw_graph():
 
             plt.savefig(f"{selected_currency}.png")
 
+async def currency(update: Update, context: CallbackContext):
+    selected_currency = context.user_data.get('selected_currency')
 
-async def get_graph(update: Update, context: CallbackContext):
-    with open(f"{selected_currency}.png", 'rb') as photo_file:
-        await update.message.reply_photo(photo=photo_file)
+    if selected_currency:
+        currency_cache_data = load_rate_data_from_cache()
+        if selected_currency in currency_cache_data:
+            last_rate = currency_cache_data[selected_currency]['rates'][-1]
+
+            with open(f"{selected_currency}.png", 'rb') as photo_file:
+                caption_text = f"\U0001F3C1 Последнее значение курса {selected_currency}: {last_rate}"
+                await update.message.reply_photo(photo=photo_file, caption=caption_text)
+        else:
+            await update.message.reply_text("\U0000274C Нет данных о курсе. Начните мониторинг с помощью /monitor_start.")
+    else:
+        await update.message.reply_text("\U0000274C Не выбрана валюта для мониторинга.\n\nИспользуйте /settings.")
 
 def main():
     print("Bot started!")
@@ -216,8 +215,8 @@ def main():
 
     application.add_handler(conv_handler)
     application.add_handler(CommandHandler("monitor_start", monitor_start))
-    application.add_handler(CommandHandler("get_currency", get_currency))
-    application.add_handler(CommandHandler("get_graph", get_graph))
+    application.add_handler(CommandHandler("currency", currency))
+    application.add_handler(CommandHandler("cancel", cancel))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
